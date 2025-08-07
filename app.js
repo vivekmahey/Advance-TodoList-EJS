55254d0a15f07b2d2e75a04859f96766808228cb
vivekmahey <vivekmahey2244@gmail.com>
2025-08-07 22:25:38 +0530

    Setup project with Express, MongoDB, EJS & initial routes

---
+++ app.js
@@ -0,0 +1,32 @@
+const express=require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv=require('dotenv');
+const methodOverride = require('method-override');   //for PUT/DELETE via forms
+const app=express();
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlPaser:true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log("MongoDB Connected !!")
+}).catch((err)=>{
+    console.log(err)
+});
+
+// Middleware
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(methodOverride('_method'));
+
+app.set('view engine','ejs');
+
+// task routes
+const taskRoutes= require('./routes/taskRoutes');
+app.use('/', taskRoutes);
+
+const PORT = process.env.PORT ;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
